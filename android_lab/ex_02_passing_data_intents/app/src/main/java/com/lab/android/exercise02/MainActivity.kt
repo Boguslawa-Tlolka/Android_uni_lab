e1650da223b238583b10753c2131aa551107f32d
Boguslawa-Tlolka <bogusia29.11@gmail.com>
2022-02-15 18:44:29 +0100

    Add navigation

--- android_lab/ex_02_passing_data_intents/app/src/main/java/com/lab/android/exercise02/MainActivity.kt
+++ android_lab/ex_02_passing_data_intents/app/src/main/java/com/lab/android/exercise02/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.lab.android.exercise02
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView = findViewById<View>(R.id.bottomNavigation) as BottomNavigationView
+        navView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.leftButton -> startActivity(Intent(this, ButtonsActivity::class.java))
+                R.id.centerButton -> startActivity(Intent(this, DataActivity::class.java))
+                R.id.rightButton -> startActivity(Intent(this, RelativeActivity::class.java))
+            }
+            true
+        }
     }
 }

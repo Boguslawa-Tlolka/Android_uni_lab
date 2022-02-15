d1833fb4df762e53d6ac8e09aee21f70a9395b98
Boguslawa-Tlolka <bogusia29.11@gmail.com>
2022-02-15 11:21:38 +0100

    Add second activity functionality

--- android_lab/ex_01_basics/app/src/main/java/com/lab/android/basics/Activity2.kt
+++ android_lab/ex_01_basics/app/src/main/java/com/lab/android/basics/Activity2.kt
@@ -3,11 +3,18 @@ package com.lab.android.basics
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+
+        val toastButton: Button = findViewById(R.id.toastButton)
+        toastButton.setOnClickListener {
+            Toast.makeText(this,getString(R.string.toast_act_2), Toast.LENGTH_SHORT).show()
+        }
     }
 
     fun finishActivity2(view: View?){

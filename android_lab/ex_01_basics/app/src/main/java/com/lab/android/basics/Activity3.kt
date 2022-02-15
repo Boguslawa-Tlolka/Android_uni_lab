a89af30b2e6014725f6279a095a20c525462b857
Boguslawa-Tlolka <bogusia29.11@gmail.com>
2022-02-15 11:17:41 +0100

    Change theme

--- android_lab/ex_01_basics/app/src/main/java/com/lab/android/basics/Activity3.kt
+++ android_lab/ex_01_basics/app/src/main/java/com/lab/android/basics/Activity3.kt
@@ -1,10 +1,11 @@
 package com.lab.android.basics
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class Activity3 : AppCompatActivity() {
+class Activity3 : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_3)

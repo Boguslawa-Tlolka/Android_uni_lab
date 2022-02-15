23a7fd86b71c6f2cc504a7f82880075dcf7b88ab
Boguslawa-Tlolka <bogusia29.11@gmail.com>
2022-02-15 10:07:21 +0100

    Add navigation between activities

--- android_lab/ex_01_basics/app/src/main/java/com/lab/android/basics/Activity2.kt
+++ android_lab/ex_01_basics/app/src/main/java/com/lab/android/basics/Activity2.kt
@@ -2,10 +2,15 @@ package com.lab.android.basics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+    }
+
+    fun finishActivity2(view: View?){
+        finish()
     }
 }
--- android_lab/ex_01_basics/app/src/main/java/com/lab/android/basics/MainActivity.kt
+++ android_lab/ex_01_basics/app/src/main/java/com/lab/android/basics/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.lab.android.basics
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button1: Button = findViewById(R.id.button1)
+        button1.setOnClickListener {
+            val myIntent1 = Intent(this, Activity1::class.java)
+            startActivity(myIntent1)
+        }
+
+        val button2: Button = findViewById(R.id.button2)
+        button2.setOnClickListener {
+            val myIntent2 = Intent(this, Activity2::class.java)
+            startActivity(myIntent2)
+        }
+
+        val button3: Button = findViewById(R.id.button3)
+        button3.setOnClickListener {
+            val myIntent3 = Intent(this, Activity3::class.java)
+            startActivity(myIntent3)
+        }
     }
 }

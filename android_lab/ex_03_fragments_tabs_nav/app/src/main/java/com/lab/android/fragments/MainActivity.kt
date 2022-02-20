9bb085bb2833daa6a6e51b63a6e1a3fc9ad61e71
Boguslawa-Tlolka <bogusia29.11@gmail.com>
2022-02-20 17:32:28 +0100

    Add tabs content and nav

--- android_lab/ex_03_fragments_tabs_nav/app/src/main/java/com/lab/android/fragments/MainActivity.kt
+++ android_lab/ex_03_fragments_tabs_nav/app/src/main/java/com/lab/android/fragments/MainActivity.kt
@@ -2,10 +2,26 @@ package com.lab.android.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavView = findViewById<View>(R.id.bottomNav) as BottomNavigationView
+        bottomNavView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.item1 -> navController.navigate(R.id.action_global_mainFragment)
+                R.id.item2 -> navController.navigate(R.id.action_global_settingsFragment)
+                R.id.item3 -> TODO()
+                R.id.item4 -> TODO()
+            }
+            true
+        }
     }
 }
--- android_lab/ex_03_fragments_tabs_nav/app/src/main/java/com/lab/android/fragments/SettingsFragment.kt
+++ android_lab/ex_03_fragments_tabs_nav/app/src/main/java/com/lab/android/fragments/SettingsFragment.kt
@@ -1,10 +1,13 @@
 package com.lab.android.fragments
 
+import android.content.Context
+import android.content.res.TypedArray
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
@@ -20,13 +23,14 @@ class SettingsFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    private val tabIcons = intArrayOf(
+   /* private val tabIcons = arrayOf(
         R.drawable.ic_person,
         R.drawable.ic_colors,
         R.drawable.ic_like_dislike
-    )
+    )*/
 
-    private val tabTitles = resources.getStringArray(R.array.tabs_titles)
+    private lateinit var tabTitles: Array<String>
+    private lateinit var tabIcons: TypedArray
 
     private lateinit var viewPagerAdapter: ViewPagerAdapter
     private lateinit var tabLayout: TabLayout
@@ -47,13 +51,23 @@ class SettingsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        tabIcons = requireActivity().resources.obtainTypedArray(R.array.tab_icons)//getIntArray(R.array.tab_icons)
+        tabTitles = requireActivity().resources.getStringArray(R.array.tabs_titles)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         tabLayout = view.findViewById(R.id.tabLayout)
         viewPager = view.findViewById(R.id.viewPager)
+        viewPagerAdapter = ViewPagerAdapter(requireActivity())
+        viewPager.adapter = viewPagerAdapter
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = tabTitles[position]
+           tab.text = tabTitles[position]
+            tab.icon = tabIcons.getDrawable(position)
         }.attach()
     }
 

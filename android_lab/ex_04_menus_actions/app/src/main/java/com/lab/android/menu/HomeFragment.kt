fd40e5835ba01db782959a15a8023714a7877dec
Boguslawa-Tlolka <bogusia29.11@gmail.com>
2022-02-21 18:18:31 +0100

    Add home fragment

---
+++ android_lab/ex_04_menus_actions/app/src/main/java/com/lab/android/menu/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.lab.android.menu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val buttonText: Button = view.findViewById(R.id.textsButton)
+        val buttonImage: Button = view.findViewById(R.id.imagesButton)
+
+    }
+
+}

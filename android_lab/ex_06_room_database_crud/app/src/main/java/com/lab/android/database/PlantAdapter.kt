4ac97ed76e6f8e0426f804f687cdafeb2f2bf6a3
Boguslawa-Tlolka <bogusia29.11@gmail.com>
2022-02-27 18:41:50 +0100

    Add plant list

---
+++ android_lab/ex_06_room_database_crud/app/src/main/java/com/lab/android/database/PlantAdapter.kt
@@ -0,0 +1,41 @@
+package com.lab.android.database
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class PlantAdapter : RecyclerView.Adapter<PlantAdapter.PlantViewHolder>() {
+
+    private var plantList = emptyList<Plant>()
+    private var age = "Age: "
+
+    class PlantViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlantViewHolder {
+        return PlantViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: PlantViewHolder, position: Int) {
+        val currentItem = plantList[position]
+        holder.itemView.nameTextView.text = currentItem.name
+        val ageText = age + currentItem.age.toString()
+        holder.itemView.ageTextView.text = ageText
+        if (currentItem.place == "Garden")
+            holder.itemView.placeImage.setImageResource(R.drawable.ic_garden)
+        else holder.itemView.placeImage.setImageResource(R.drawable.ic_house)
+        currentItem.color?.let { holder.itemView.placeImage.setColorFilter(it) }
+    }
+
+    override fun getItemCount(): Int {
+        return plantList.size
+    }
+
+    fun setData(plantList: List<Plant>) {
+        this.plantList = plantList
+        notifyDataSetChanged()
+    }
+}

1a3017465c7e6cb7c9ad7d05d5f8640740e046cc
Boguslawa-Tlolka <bogusia29.11@gmail.com>
2022-02-27 17:56:45 +0100

    Add repository

---
+++ android_lab/ex_06_room_database_crud/app/src/main/java/com/lab/android/database/PlantRepository.kt
@@ -0,0 +1,25 @@
+package com.lab.android.database
+
+import androidx.lifecycle.LiveData
+
+class PlantRepository (private val plantDao: PlantDao) {
+
+    val readAllData: LiveData<List<Plant>> = plantDao.readAllPlants()
+
+    suspend fun addPlant(plant: Plant){
+        plantDao.addPlant(plant)
+    }
+
+    suspend fun updatePlant(plant: Plant){
+        plantDao.updatePlant(plant)
+    }
+
+    suspend fun deletePlant(plant: Plant){
+        plantDao.deletePlant(plant)
+    }
+
+    suspend fun deleteAllPlants(){
+        plantDao.deleteAllPlants()
+    }
+
+}

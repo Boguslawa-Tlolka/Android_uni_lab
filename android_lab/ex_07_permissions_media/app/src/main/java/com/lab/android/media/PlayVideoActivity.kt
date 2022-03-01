a849425cd3ccf5ff73d41464b4164d588a86bcdd
Boguslawa-Tlolka <bogusia29.11@gmail.com>
2022-03-01 10:46:41 +0100

    Add navigation

---
+++ android_lab/ex_07_permissions_media/app/src/main/java/com/lab/android/media/PlayVideoActivity.kt
@@ -0,0 +1,32 @@
+package com.lab.android.media
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class PlayVideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_video)
+
+        val audioButton: Button = findViewById(R.id.recordAudioButton)
+        val playButton: Button = findViewById(R.id.playVideoButton)
+        val recordVideoButton: Button = findViewById(R.id.recordVideoButton)
+
+        audioButton.setOnClickListener {
+            val intent = Intent(this, RecordAudioActivity::class.java)
+            startActivity(intent)
+        }
+
+        playButton.setOnClickListener {
+            val intent = Intent(this, PlayVideoActivity::class.java)
+            startActivity(intent)
+        }
+
+        recordVideoButton.setOnClickListener {
+            val intent = Intent(this, RecordVideoActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}

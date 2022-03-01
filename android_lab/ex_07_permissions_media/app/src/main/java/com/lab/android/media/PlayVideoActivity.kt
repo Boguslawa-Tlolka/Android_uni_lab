c4926a36f60c901b66f703cc0f47552cb540e6f5
Boguslawa-Tlolka <bogusia29.11@gmail.com>
2022-03-01 11:24:57 +0100

    Add video play

--- android_lab/ex_07_permissions_media/app/src/main/java/com/lab/android/media/PlayVideoActivity.kt
+++ android_lab/ex_07_permissions_media/app/src/main/java/com/lab/android/media/PlayVideoActivity.kt
@@ -1,15 +1,41 @@
 package com.lab.android.media
 
-import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.widget.MediaController
+import android.widget.Toast
+import android.widget.VideoView
 
 class PlayVideoActivity : AppCompatActivity() {
+
+    var simpleVideoView: VideoView? = null
+    var mediaController: MediaController? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_video)
 
+        simpleVideoView = findViewById<View>(R.id.simpleVideoView) as VideoView
 
+        if (mediaController == null) {
+            mediaController = MediaController(this)
+            mediaController!!.setAnchorView(this.simpleVideoView)
+        }
+
+        simpleVideoView!!.setMediaController(mediaController)
+
+        simpleVideoView!!.setVideoURI(
+            Uri.parse("android.resource://" + packageName + "/" + R.raw.seals_video)
+        )
+
+        simpleVideoView!!.requestFocus()
+        simpleVideoView!!.start()
+
+        simpleVideoView!!.setOnErrorListener { _, _, _ ->
+            Toast.makeText(applicationContext, "Error occurred!", Toast.LENGTH_LONG).show()
+            false
+        }
     }
 }
